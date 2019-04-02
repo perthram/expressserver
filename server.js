3545af8cd374a11adc035f5e6e0589da5d0c0749
Shriram Chandak <Shriram.Chandak@e2open.com>
2019-04-02 15:38:15 +0530

    prepared for deploy

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-const fileUpload = require('./routes/api/fileUpload');
+//const fileUpload = require('./routes/api/fileUpload');
 
 const app = express();
 
@@ -34,6 +35,16 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 //app.use('/api/fileUpload', fileUpload);
+
+//Serve static assests if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
